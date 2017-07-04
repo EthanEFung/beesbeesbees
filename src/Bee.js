d9c04436399eb8dbc9a91163f1d81e8883fe2039
Ethan Fung <EthanEFung@gmail.com>
2017-07-04 08:03:38 -0700

    Write operating BeesBeesBees source code
    Things to remember from this sprint:
    (1)use .call method within the scope of the
    psuedoclassical constructor in order to have
    access to the properties and methods of the
    superclass.
    
    (2)Set the prototype to point to an instance
    of the superclass prototype. Avoid having the
    prototype point to an instance of 'new
    Superclass' since the new keyword requires
    any arguments of the instance to have been
    defined upon inception.
    
    (3)Don't forget to set the prototype's
    constructor to point back at the constructor
    function.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+//For shared properties with the superclass, declare an instance of
+//the Grub prototype, using Object.create.
+Bee.prototype = Object.create(Grub.prototype);
+
+//Remember to point the constructor property back at the Bee function.
+Bee.prototype.constructor = Bee;
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+//Note to self: cannot set the prototype using 'new Bee' since it will
+//not allow us to accept arguments that haven't yet been defined.
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function()  {
+  this.treasureChest.push('treasure');
+}
 
